45bb8ee9df5ad8de32de3885ffcaaea0fb873956
Forstsaw <43381115+Forstsaw@users.noreply.github.com>
2019-10-23 21:13:36 +0700

    update convert binary to string
    add convertStringToBinary and convertTo8bit function
    add convert binary to string

--- Stream Cipher.py
+++ Stream Cipher.py
@@ -1,15 +1,33 @@
 import random
 
 
-rawText = "Forstsaw"
-m = ''.join(format(ord(x), 'b') for x in rawText) # convert plaint text to binary
+rawText = "hello world"
 key = "" # The Key for encrypt or decrypt
+
+# convert plaint text to binary
+def convertStringToBinary(m): 
+    return ' '.join(format(ord(x), 'b') for x in m).split(" ")
+m = convertStringToBinary(rawText)
 
 
 #generate random binary key
 for i in range(len(m)):
 	key += str(random.randint(0,1))
 
+
+
+# add 0 to every binary until 8 bit
+def convertTo8bit(binary):
+    binary_8bit =""
+    for i in binary:
+        temp = i
+        while len(temp) != 8:
+            temp = '0'+temp
+        binary_8bit += temp
+        
+    return binary_8bit
+m = convertTo8bit(m)
+key = convertTo8bit(key)
 
 #encrypt the message with the key 
 def encrypt(m,k):
@@ -28,10 +46,11 @@ def decrypt(c,k):
 
 c = encrypt(m,key)
 d = decrypt(c,key)
-
+plainDec = ''.join(chr(int(x, 2)) for x in (d[8*i:8*i+8] for i in range(len(d)//8))) #convert binary to ascii
 print("\t\tSTREAM CIPHER ALGORITHM BY FORSTSAW\n")
 print("Message (Plain Text) : ", rawText)
 print("Binary: ", m)
 print("Key : ", key)	
 print("Encryption : ", c)
 print("Decryption : ", d)
+print("Plaint text (Decryption): ",plainDec)
